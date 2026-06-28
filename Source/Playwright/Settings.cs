bb392e45c2dfde57f041e16df2823dffc617dc11
morerokk <hugo.zink@hotmail.com>
2026-06-28 12:48:33 +0200

    chore: change default setting of scenario edit button

--- Source/Playwright/Settings.cs
+++ Source/Playwright/Settings.cs
@@ -7,7 +7,7 @@ namespace Rokk.Playwright
     {
         public bool EnablePlaywrightButtonInMainMenu = false;
         public bool EnablePlaywrightButtonInScenarioSelect = true;
-        public bool EnablePlaywrightButtonInScenarioEditor = false;
+        public bool EnablePlaywrightButtonInScenarioEditor = true;
         public bool HideReplacedFactions = true;
         public bool HideDryadsInAnimals = true;
 
@@ -16,7 +16,7 @@ namespace Rokk.Playwright
             base.ExposeData();
             Scribe_Values.Look(ref EnablePlaywrightButtonInMainMenu, nameof(EnablePlaywrightButtonInMainMenu), false);
             Scribe_Values.Look(ref EnablePlaywrightButtonInScenarioSelect, nameof(EnablePlaywrightButtonInScenarioSelect), true);
-            Scribe_Values.Look(ref EnablePlaywrightButtonInScenarioEditor, nameof(EnablePlaywrightButtonInScenarioEditor), false);
+            Scribe_Values.Look(ref EnablePlaywrightButtonInScenarioEditor, nameof(EnablePlaywrightButtonInScenarioEditor), true);
             Scribe_Values.Look(ref HideReplacedFactions, nameof(HideReplacedFactions), true);
             Scribe_Values.Look(ref HideDryadsInAnimals, nameof(HideDryadsInAnimals), true);
         }
@@ -25,7 +25,7 @@ namespace Rokk.Playwright
         {
             EnablePlaywrightButtonInMainMenu = false;
             EnablePlaywrightButtonInScenarioSelect = true;
-            EnablePlaywrightButtonInScenarioEditor = false;
+            EnablePlaywrightButtonInScenarioEditor = true;
             HideReplacedFactions = true;
             HideDryadsInAnimals = true;
         }
